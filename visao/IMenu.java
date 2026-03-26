ff03091d41c73bac43460d13a1da761cbb0a582b
pdrchagas <pedro.c.n.f.nascimento@gmail.com>
2026-03-26 12:17:03 -0300

    Add files via upload

--- visao/IMenu.java
+++ visao/IMenu.java
@@ -1,1 +1,10 @@
+package visao;
 
+import controle.CadastroAlunos;
+
+/**
+ * Interface para garantir o padrao de entradas e saidas.
+ */
+public interface IMenu {
+    void exibirMenu(CadastroAlunos cadastro);
+}
